2305062265d06232602b31b344236ab69e5ef8f8
TylerPassyn <Tyler.A.Passyn@gmail.com>
2025-03-27 20:19:06 -0400

    Project Cards Update, we have got two of them

--- my-app/src/components/Projects.jsx
+++ my-app/src/components/Projects.jsx
@@ -24,6 +24,24 @@ const Projects = () => {
 						</div>
 					</div>
 				</div>
+				<div className="column">
+					<div className="Project-card">
+						<img
+							src="https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b"
+							alt="Project"
+							style={{ width: "100%" }}
+						/>
+						<div className="inside-card-container">
+							<h3>Project 1</h3>
+							<div className="card-info">
+								<p>Project Description</p>
+							</div>
+							<Link to="/" className="btn-project">
+								View Project
+							</Link>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
