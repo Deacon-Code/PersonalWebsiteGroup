13eb466413e0f01ebbcff4ccea914698d2b214a0
Tyler Passyn <Tyler.A.Passyn@gmail.com>
2025-04-21 18:30:46 -0400

    some stuff

--- my-app/src/routes/Projects/Project_1.jsx
+++ my-app/src/routes/Projects/Project_1.jsx
@@ -20,8 +20,8 @@ const Project_1 = () => {
             heading="Fleeing to Russia"
             text1="I Fled to Russia to ensure I could cover my tail when the CIA came after me. I totally am not on Putin's payroll and my family is not in state prision."
             image1 = {Mother_Russia}
-            text2="I Fled to Russia to ensure I could cover my tail when the CIA came after me. I totally am not on Putin's payroll and my family is not in state prision."
-            image2 = {Mother_Russia}
+            text2=" Anyway, it is pretty cold here. The food is okay, the people are nice, and the vodka is strong. I am not sure how long I will be here, but I am sure it will be a while."
+            image2 = "https://www.telegraph.co.uk/multimedia/archive/02633/ed_2633795b.jpg"
         />
       <Footer/>
     </div>
